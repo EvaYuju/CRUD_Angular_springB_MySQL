a57552faf474db975278f2cc61ccae51677bc2a5
Eva <elopezmarquez@safareyes.es>
2025-01-21 22:49:08 +0100

    first commit

---
+++ crud/crud/src/test/java/com/angular_springb_sql/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular_springb_sql.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
