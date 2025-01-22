931c40817ef15991100866aa017440d8b73a4488
Eva <elopezmarquez@safareyes.es>
2025-01-22 23:45:30 +0100

    ProductoDTO (sin id) + dependencia restricciones

--- crud/crud/src/main/java/dto/ProductoDto.java
+++ crud/crud/src/main/java/dto/ProductoDto.java
@@ -1,4 +1,22 @@
 package dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 // Patron de diseño Data Transfer Object
 public class ProductoDto {
+    // Restincciones
+    @NotBlank
+    private String nombre;
+    @Min(0)
+    private float precio;
+
+    public ProductoDto() {
+    }
+
+    public ProductoDto(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
 }
