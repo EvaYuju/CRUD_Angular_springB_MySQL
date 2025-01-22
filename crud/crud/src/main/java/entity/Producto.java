0eee7bd81a5c631eab6b97a59335ef4c96d2ff98
Eva <elopezmarquez@safareyes.es>
2025-01-22 23:11:17 +0100

    Entidad Producto.java

--- crud/crud/src/main/java/entity/Producto.java
+++ crud/crud/src/main/java/entity/Producto.java
@@ -1,4 +1,47 @@
 package entity;
 
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name = "Producto")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private float precio;
+
+    // Constructores
+    public Producto() {
+    }
+
+    public Producto(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
 }
