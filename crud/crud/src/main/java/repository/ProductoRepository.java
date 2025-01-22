f9ff90cbc3d9023172173b36044671a9ea08abb6
Eva <elopezmarquez@safareyes.es>
2025-01-22 23:21:56 +0100

    Repository

--- crud/crud/src/main/java/repository/ProductoRepository.java
+++ crud/crud/src/main/java/repository/ProductoRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
-public class ProductoRepository {
+import entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> { // Tipo de dato y tipo de dato de la PK
+    Optional<Producto> findByNombre(String nombre);
+    boolean existByNombre(String nombre);
 }
