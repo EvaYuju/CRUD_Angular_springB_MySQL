c90a55670e656d4caedb3c1634a6fbc4891a64a5
Eva <elopezmarquez@safareyes.es>
2025-01-22 23:40:20 +0100

    Clase Mensaje.java para mostrar en pantalla cliente

--- crud/crud/src/main/java/dto/Mensaje.java
+++ crud/crud/src/main/java/dto/Mensaje.java
@@ -1,4 +1,18 @@
 package dto;
 
 public class Mensaje {
+    // Para mostrar mensajes por pantalla en el cliente
+    public String mensaje;
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
 }
