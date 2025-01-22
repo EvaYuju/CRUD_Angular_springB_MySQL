deb78279afc629609e6fe00e1a5f999d6c327029
Eva <elopezmarquez@safareyes.es>
2025-01-22 23:36:06 +0100

    Servicio + metodos

--- crud/crud/src/main/java/service/ProductoService.java
+++ crud/crud/src/main/java/service/ProductoService.java
@@ -1,4 +1,40 @@
 package service;
 
+import entity.Producto;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.ProductoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
 public class ProductoService {
+    @Autowired // Cuando sea necesario que se encargue Spring de crear una instancia del Repositorio
+    ProductoRepository productoRepository;
+    public List<Producto> list(){
+        return productoRepository.findAll(); // Devuelve todos los registros de esa tabla
+    }
+    public Optional<Producto> getOne(int id){
+        return productoRepository.findById(id); // Devuelve por el id
+    }
+    public Optional<Producto> getByNombre(String nombre){
+        return productoRepository.findByNombre(nombre); // Devuelve por el nombre
+    }
+    public void save(Producto producto){
+        productoRepository.save(producto);
+    }
+    public void delete(int id){
+        productoRepository.deleteById(id);
+    }
+    public boolean existsById(int id){
+        return productoRepository.existsById(id);
+    }
+    public boolean existsByNombre(String nombre){
+        return productoRepository.existByNombre(nombre);
+    }
+
+
 }
