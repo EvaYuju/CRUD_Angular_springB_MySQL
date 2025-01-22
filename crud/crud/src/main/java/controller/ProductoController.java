8e251201c77e60df54858392ac0fe0a5f9778a2b
Eva <elopezmarquez@safareyes.es>
2025-01-23 00:03:31 +0100

    ProductoController.java mapping

--- crud/crud/src/main/java/controller/ProductoController.java
+++ crud/crud/src/main/java/controller/ProductoController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import dto.Mensaje;
+import entity.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.ProductoService;
+
+import java.util.List;
+
+@RestController // Indicamos que es una APIRest
+@RequestMapping("/producto") // url localhost:8080/producto
+//@CrossOrigin(origins="*") // * servicio desde cualquier url
+@CrossOrigin(origins="http://localhost:4200") // servicio en el puerto de angular
 public class ProductoController {
+    @Autowired
+    ProductoService productoService;
+
+    @GetMapping("/lista") //url
+    public ResponseEntity<List<Producto>> list() {
+        List<Producto> list = productoService.list();
+        // return new ResponseEntity<List<Producto>>(list, HttpStatus.OK);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Producto> getById(@PathVariable("id") int id){
+        if(!productoService.existsById(id))
+            return new ResponseEntity(new Mensaje("No existe"), HttpStatus.NOT_FOUND);
+        // Si existe creramos un producto
+        Producto producto = productoService.getOne(id).get();
+        return new ResponseEntity(producto, HttpStatus.OK);
+    }
 }
