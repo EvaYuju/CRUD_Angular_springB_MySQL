884970fe86edb863739e9f418585378e5d6de37b
Eva <elopezmarquez@safareyes.es>
2025-01-28 00:41:53 +0100

    no creaba la tabla - arreglado

--- crud/crud/src/main/java/com/angular_springb_sql/crud/CrudApplication.java
+++ crud/crud/src/main/java/com/angular_springb_sql/crud/CrudApplication.java
@@ -2,8 +2,10 @@ package com.angular_springb_sql.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "entity") // Reemplaza con tu paquete real
 public class CrudApplication {
 
 	public static void main(String[] args) {
--- crud/crud/src/main/java/entity/Producto.java
+++ crud/crud/src/main/java/entity/Producto.java
@@ -3,7 +3,7 @@ package entity;
 import jakarta.persistence.*;
 
 @Entity
-//@Table(name = "Producto")
+@Table(name = "Producto")
 public class Producto {
 
     @Id
@@ -24,23 +24,18 @@ public class Producto {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public float getPrecio() {
         return precio;
     }
-
     public void setPrecio(float precio) {
         this.precio = precio;
     }
