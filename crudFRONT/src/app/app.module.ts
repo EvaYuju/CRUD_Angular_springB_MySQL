b172a10044de7c17831ab9f4428001c7381c6594
Eva <elopezmarquez@safareyes.es>
2025-01-30 00:10:54 +0100

    importados httpClient y forms

--- crudFRONT/src/app/app.module.ts
+++ crudFRONT/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';  // Importa el RouterModule
 
+import { provideHttpClient } from '@angular/common/http';
+import { withInterceptorsFromDi } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
 // External
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
@@ -13,12 +17,17 @@ import { ToastrModule } from 'ngx-toastr';
   ],
   imports: [
     AppRoutingModule,  
+    
+    FormsModule,
     CommonModule,
     RouterModule,  
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), AppRoutingModule, // ToastrModule added
   ],
-  providers: []
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
+
+  ]
  // bootstrap: []   Componente principal para arrancar la app
 
 })
