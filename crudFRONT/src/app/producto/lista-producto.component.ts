7c6ab6db24599eb4135a0f2af3391fe8657d4aba
Eva <elopezmarquez@safareyes.es>
2025-01-29 23:49:04 +0100

    creada clase Producto y componentes y servicio

---
+++ crudFRONT/src/app/producto/lista-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-producto',
+  imports: [],
+  templateUrl: './lista-producto.component.html',
+  styleUrl: './lista-producto.component.css'
+})
+export class ListaProductoComponent {
+
+}
---
+++ crudFRONT/src/app/producto/nuevo-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-producto',
+  imports: [],
+  templateUrl: './nuevo-producto.component.html',
+  styleUrl: './nuevo-producto.component.css'
+})
+export class NuevoProductoComponent {
+
+}
