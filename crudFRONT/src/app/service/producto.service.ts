eaf856c2f18c4a61d5a361bee62363ef6a15ef1d
Eva <elopezmarquez@safareyes.es>
2025-01-30 00:31:59 +0100

    añadiendo backEnd métodos al servicio

--- crudFRONT/src/app/service/producto.service.ts
+++ crudFRONT/src/app/service/producto.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
+
 /* Consume un APIRest-el service se conecta directamente y obtiene todo */
 @Injectable({
   providedIn: 'root'
@@ -13,4 +16,12 @@ export class ProductoService {
 
   /* En el constructor vamos a inyectar el HttpClient pero como tengo provideHttpClient en el app.module no sería necesario */
   constructor(private httpClient: HttpClient) { }
+
+  // Métodos:
+  /* Este método lista nos va a devolver un Observable<claseProducto[lista]
+  que nos devuelva con el metodo get la lista de producto */
+  public lista(): Observable<Producto[]> {
+    return this.httpClient.get<Producto[]>(this.productoURL + 'lista');
+  }
+
 }
