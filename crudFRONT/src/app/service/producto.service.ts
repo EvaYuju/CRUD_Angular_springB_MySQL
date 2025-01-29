9d9300960c6c61f78690c078a85661f6c6983a2f
Eva <elopezmarquez@safareyes.es>
2025-01-30 00:24:22 +0100

    productoURL

--- crudFRONT/src/app/service/producto.service.ts
+++ crudFRONT/src/app/service/producto.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+/* Consume un APIRest-el service se conecta directamente y obtiene todo */
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
+  /* En el BackEnd se consume el API Rest para obtener los datos de la BD
+   y mostrarlos en la pantalla del FrontEnd  */
 
-  constructor() { }
+  /* BackEnd -> ProductoController en el @RequestMapping la url es "/producto"*/
+  productoURL = 'http://localhost:8080/producto/';
+
+  /* En el constructor vamos a inyectar el HttpClient pero como tengo provideHttpClient en el app.module no sería necesario */
+  constructor(private httpClient: HttpClient) { }
 }
