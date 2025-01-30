9c08fd84b1bc04da3f21520a79e33221af1f6f23
Eva <elopezmarquez@safareyes.es>
2025-01-30 20:02:12 +0100

    <3 - servicio para comunicar con APIRest

--- crudFRONT/src/app/service/producto.service.ts
+++ crudFRONT/src/app/service/producto.service.ts
@@ -11,7 +11,7 @@ export class ProductoService {
   /* En el BackEnd se consume el API Rest para obtener los datos de la BD
    y mostrarlos en la pantalla del FrontEnd  */
 
-  /* BackEnd -> ProductoController en el @RequestMapping la url es "/producto"*/
+  /* nuestra url -> ProductoController en el @RequestMapping la url es "/producto"*/
   productoURL = 'http://localhost:8080/producto/';
 
   /* En el constructor vamos a inyectar el HttpClient pero como tengo provideHttpClient en el app.module no sería necesario */
@@ -19,9 +19,43 @@ export class ProductoService {
 
   // Métodos:
   /* Este método lista nos va a devolver un Observable<claseProducto[lista]
-  que nos devuelva con el metodo get la lista de producto */
+  que nos devuelva con el metodo .get la lista de producto */
   public lista(): Observable<Producto[]> {
     return this.httpClient.get<Producto[]>(this.productoURL + 'lista');
   }
+
+  /* Este método detalle nos va a devolver un Observable<claseProducto[detalle]
+  que nos devuelva con el metodo .get el detalle de producto */
+  public detail(id: number): Observable<Producto> {       
+    // Se le pasa el id del producto
+    return this.httpClient.get<Producto>(this.productoURL + `detail/${id}`); // Se usa el acento grave para concatenar ``
+  }
+
+  /* Metodo para buscar un producto por su nombre */
+  public detailName(nombre: string): Observable<Producto> {
+    /* Se le pasa el nombre del producto y se le dice que es de tipo Producto */
+    return this.httpClient.get<Producto>(this.productoURL + `detailName/${nombre}`);
+  }
+
+  /* Metodo para crear un producto */
+  public create(producto: Producto): Observable<any> {
+    /* Se le dice que es de tipo any; se le pasa el objeto producto porque post tiene requestBody */
+    return this.httpClient.post<any>(this.productoURL + 'create', producto);
+  }
+
+  /* Metodo para editar un producto */
+  public update(id: number, producto: Producto): Observable<any> {
+    /* Se le pasa el id del producto y el objeto producto porque PUT tiene requestBody */
+    return this.httpClient.put<any>(this.productoURL + `update/${id}`, producto);
+  }
+
+  /* Metodo para borrar un producto */  
+  public delete(id: number): Observable<any> {
+    /* Se le pasa el id del producto, método delete */
+    return this.httpClient.delete<any>(this.productoURL + `delete/${id}`);
+  }   
+
+
+
 
 }
